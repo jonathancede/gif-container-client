8e75961641095853f73920f50852a01432a71cf1
Jonathan Cedeño <jony.cede@gmail.com>
2021-10-27 23:27:43 +0200

    added tag fail in random gif

--- src/services/giphy.js
+++ src/services/giphy.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export function getRandomGifGiphy() {
   return axios({
     method: "GET",
-    url: `https://api.giphy.com/v1/gifs/random?api_key=${process.env.REACT_APP_GIPHY_API_KEY}`,
+    url: `https://api.giphy.com/v1/gifs/random?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&tag=fail`,
   });
 }
